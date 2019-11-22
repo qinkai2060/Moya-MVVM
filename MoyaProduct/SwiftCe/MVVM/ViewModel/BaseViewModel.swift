f222bf042a44370ed0f292f950a9ab5d935f9217
qinkai <qinkai@ijiuyue.com>
2019-11-22 15:31:58 +0800

    测试

---
+++ MoyaProduct/SwiftCe/MVVM/ViewModel/BaseViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewModel.swift
+//  SwiftCe
+//
+//  Created by usermac on 2019/10/12.
+//  Copyright © 2019 usermac. All rights reserved.
+//
+
+import UIKit
+import Moya
+import RxSwift
+import RxCocoa
+protocol baseViewModelDelegate {
+    func hh_setupView ()
+    
+    func hh_bindView ()
+}
+extension baseViewModelDelegate {
+    func hh_setupView() {
+        
+    }
+    func hh_bindView() {
+        
+    }
+}
+class BaseViewModel: NSObject {
+    
+
+    
+}
---
+++ MoyaProduct/SwiftCeTests/SwiftCeTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftCeTests.swift
+//  SwiftCeTests
+//
+//  Created by usermac on 2019/10/9.
+//  Copyright © 2019 usermac. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftCe
+
+class SwiftCeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
